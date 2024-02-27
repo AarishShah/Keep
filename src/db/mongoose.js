52ef6b76797d7cb40d88cd813f6e5195807ca0d9
mohsin6837 <68821359+mohsin6837@users.noreply.github.com>
2024-02-27 19:15:36 +0530

    Solved all fetch issues (#1)
    fixed the issue of fetching by updating the url to root

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+//require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 const mongoose = require('mongoose')
 
 // Construct the MongoDB Atlas connection URI
@@ -7,18 +6,10 @@ const username = encodeURIComponent(process.env.DB_USERNAME);
 const password = encodeURIComponent(process.env.DB_PASSWORD);
 const clusterUrl = process.env.DB_CLUSTER_URL;
 const dbName = 'keep';
-const uri = `mongodb+srv://${username}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority`;
-
-
+const uri = process.env.MONGODB_URI;
 mongoose.connect
     (
-        uri,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            // useCreateIndex: true
-            // useFindAndModify: false // set true by default, this will address the depracation warnings
-        }
+        uri
     ).catch(error => {
         console.error("Database connection failed:", error);
         process.exit(1); // Optionally exit the process if unable to connect
